ed3172fe8f0e6aff1e1fd935033c2b0c1d1e9024
FelixVoro <115408780+FelixVoro@users.noreply.github.com>
2022-10-23 15:10:31 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, F, a1, a2, a3;
+	a1 = 1;
+	a2 = 1;
+	a3 = 2;
+	cin >> a;
+	if (a == 1 || a == 2) cout << 1;
+	else if (a == 0) cout << 0;
+	else {
+		for (int i = 3; i < a; i++)
+		{
+			a1 = a2;
+			a2 = a3;
+			a3 = a1 + a2;
+		}
+		cout << a3;
+	}
+
+}
